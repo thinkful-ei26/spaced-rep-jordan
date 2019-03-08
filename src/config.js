efa98aca422279aa4509bb6d21007e3a8d073b7d
Jordan <jordanhaddadi@gmail.com>
2019-03-08 14:01:58 -0500

    zip

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
     // TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
-    // API_BASE_URL: process.env.API_URL || 'https://spaced-repitetition-server.herokuapp.com'
-    API_BASE_URL: 'http://localhost:8080'
+    API_BASE_URL: process.env.API_URL || 'https://spaced-repitetition-server.herokuapp.com'
+    // API_BASE_URL: 'http://localhost:8080'
   };   
